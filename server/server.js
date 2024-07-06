5a8d928d8b0057fb3e77b009cc001c7c0f08a9b0
Ruan2PVS9 <ruan.souza.pv@gmail.com>
2024-07-06 17:19:02 -0300

    fix api router

--- server/server.js
+++ server/server.js
@@ -4,10 +4,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(router);
+app.use('/api', router);
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-app.use('/api', router);
