2713e7dbcfb06e24fd3f5aa26162607560606e31
Ruan2PVS9 <ruan.souza.pv@gmail.com>
2024-07-06 17:02:26 -0300

    fix src/server >> /server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = require('./src/router');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+app.use('/api', router);
--- server/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const router = require('./router');
-
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3333;
-
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
