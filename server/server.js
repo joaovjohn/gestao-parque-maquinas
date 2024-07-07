19c6a65af27c4cd3aff0902717da1d0cfdfff88f
Ruan2PVS9 <ruan.souza.pv@gmail.com>
2024-07-07 18:03:15 -0300

    autenticate

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = require('./src/router');
+const passport = require('passport');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 require('dotenv').config();
+const { db } = require('./src/config/connection');
 
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+};
+passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+    const user = db.oneOrNone(`SELECT id FROM pessoa WHERE id = $1`, jwt_payload.id); 
+    if (user) {
+    return done(null, user);
+    } else {
+    return done(null, false);
+    }
+    })
+);
 const app = express();
 app.use(express.json());
+app.use(passport.initialize());
 app.use('/api', router);
 
 const PORT = process.env.PORT || 3333;
---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,24 @@
+const jsonwebtoken = require('jsonwebtoken');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const { db } = require('../config/connection');
+require('dotenv').config();
+
+async function login(req, res) {
+    const { login, senha } = req.body;
+    if (!login || !senha) {
+        return res.status(400).json({ error: 'Login e senha são obrigatórios' });
+    }
+    const user = await db.oneOrNone(`SELECT id,login,senha FROM pessoa WHERE login = $1`, login);
+    if (user && await bcrypt.compare(senha, user.senha)) {
+        const payload = { id: user.id, login: user.login };
+        const token = jsonwebtoken.sign(payload, process.env.JWT_SECRET, { expiresIn: '4h' });
+        res.json({ message: 'Login successful', token });
+    } else {
+        res.status(401).json({ message: 'usuario ou senha invalidos' });
+    }
+}
+
+module.exports = {
+    login
+};
