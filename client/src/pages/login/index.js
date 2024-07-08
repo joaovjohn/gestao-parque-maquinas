371b4eca3080a0d0224ceddc145517cee7ed2934
Henrique Eduardo Simonato <simonato1905@gmail.com>
2024-07-08 12:58:46 -0300

    feat: :hammer: finalizado estilo da tela de login

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,5 +1,7 @@
 import React, { useState,useContext } from 'react';
 import { AuthContext } from '../../contexts/authContext'; // Ajuste o caminho conforme necessário
+import "./Login.css";
+import Logo from '../../assets/logo.gif';
 
 export function Login() {
   const [login, setlogin] = useState('');
@@ -20,20 +22,29 @@ export function Login() {
     <div className="container">
       <img src="" alt="" className="" />
       <form onSubmit={handleSubmit}>
+        
+        <header>
+          <img src={ Logo } alt="Logo da Prefeitura de São José do Inhacora/RS." className='Logo'/>
+          <h1>Gestão de Parque de Máquinas</h1>
+        </header>
+
         <div className="form-group">
           <label htmlFor="login">Usuário:</label>
           <input
             type="text"
+            placeholder="login.login"
             id="login"
             value={login}
             onChange={(e) => setlogin(e.target.value)}
             className="form-control"
           />
         </div>
+
         <div className="form-group">
           <label htmlFor="password">Senha:</label>
           <input
             type="password"
+            placeholder='*********'
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -41,7 +52,7 @@ export function Login() {
           />
         </div>
         <button type="submit" className="btn btn-primary">
-          Confirmar
+          Entrar
         </button>
       </form>
     </div>
